fc49cb872bc56ce9edbe87c04fdd39beaf197f3f
Mike Henders <mike.henders@gmail.com>
2016-02-24 12:10:54 -0700

    Add directive test

---
+++ src/client/specs/moduleA/hello.directive.spec.js
@@ -0,0 +1,19 @@
+import angularMocks from 'vendor/angular-mocks.js';
+import ModuleA from 'app/moduleA/moduleA.module.js';
+
+describe('hello world directive in moduleA', () => {
+  let element;
+  let scope;
+
+  beforeEach(angular.mock.module(ModuleA.name));
+
+  beforeEach(inject(($rootScope, $compile) => {
+    scope = $rootScope.$new();
+    element = $compile('<hello></hello>')(scope);
+  }));
+
+  it('computes the position', () => {
+    scope.$digest();
+    expect(element.html()).toContain('position is 2');
+  });
+});
