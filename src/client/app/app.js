9e44ffa012030f0847db3b619cbae1819efca4a7
Mike Henders <mike.henders@curvedental.com>
2016-02-24 09:48:00 -0700

    Enable ngStrictDi

--- src/client/app/app.js
+++ src/client/app/app.js
@@ -2,11 +2,7 @@ import angular from 'vendor/angular.js';
 import moduleA from './moduleA/moduleA.module.js';
 import { foo } from './bar.js';
 
-angular.module('app', [moduleA.name])
-  .controller('HelloController', function($scope, calculatorService) {
-    let position = calculatorService.add(1,1);
-    $scope.greeting = 'Hello, Angular Starter.  You are number ' + position;
-  });
+angular.module('app', [moduleA.name]);
 
 export function think() {
   return foo;
